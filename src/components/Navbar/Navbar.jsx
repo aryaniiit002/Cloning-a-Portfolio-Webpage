51bad535815770f68d6e9817afe43fc9475bf861
aryaniiit002 <aryanbindal2015@gmail.com>
2022-06-02 12:40:10 +0530

    Updating Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate, Link } from "react-router-dom";
+import React from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
 import { NavHashLink } from 'react-router-hash-link';
 
+import "./Navbar.css";
 
 function Navbar() {
     const location = useLocation();
@@ -10,11 +11,26 @@ function Navbar() {
     return (
         <div className="navContainer">
             <div className="navOptContainer">
-                <NavHashLink smooth to="/portfolio#home" className={`navOpt ${path === "#home" && "select"}`}>HOME</NavHashLink>
-                <NavHashLink smooth to="/portfolio#skill" className={`navOpt ${path === "#education" && "select"}`}>EDUCATION</NavHashLink>
-                <NavHashLink smooth to="/portfolio#projects" className={`navOpt ${path === "#achievement" && "select"}`}>ACHIEVEMENT</NavHashLink>
-                <NavHashLink smooth to="/portfolio#experience" className={`navOpt ${path === "#hobbies" && "select"}`}>HOBBIES</NavHashLink>
-                <NavHashLink smooth to="/portfolio#contact" className={`navOpt ${path === "#contact" && "select"}`}>CONTACT</NavHashLink>
+                <NavHashLink smooth to="/portfolio#home"
+                    className={`navOpt ${path === "#home" && "select"}`}>
+                    HOME
+                </NavHashLink>
+                <NavHashLink smooth to="/portfolio#education"
+                    className={`navOpt ${path === "#education" && "select"}`}>
+                    EDUCATION
+                </NavHashLink>
+                <NavHashLink smooth to="/portfolio#achievement"
+                    className={`navOpt ${path === "#achievement" && "select"}`}>
+                    ACHIEVEMENT
+                </NavHashLink>
+                <NavHashLink smooth to="/portfolio#hobbies"
+                    className={`navOpt ${path === "#hobbies" && "select"}`}>
+                    HOBBIES
+                </NavHashLink>
+                <NavHashLink smooth to="/portfolio#contact"
+                    className={`navOpt ${path === "#contact" && "select"}`}>
+                    CONTACT
+                </NavHashLink>
             </div>
         </div>
     )
